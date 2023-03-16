a732b9df03d6eaffdadf5163a25abc5d9a5b60fc
Matias <matiasc.02.2003@gmail.com>
2023-03-15 21:50:44 -0300

    Connect to new deploy back-end 🌌

--- src/services/API_URL.js
+++ src/services/API_URL.js
@@ -1,2 +1,2 @@
-export const API_URL = 'http://localhost:3001/dogs'
-export const API_URL_TEMPERAMENTS = 'http://localhost:3001/temperaments'
+export const API_URL = 'https://dogs-api-production-f5e8.up.railway.app/dogs'
+export const API_URL_TEMPERAMENTS = 'https://dogs-api-production-f5e8.up.railway.app/temperaments'
